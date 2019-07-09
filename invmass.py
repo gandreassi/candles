b6a3fcd102f7920747fba6cdcb921a7ee24334fc
Guido <guido.andreassi@cern.ch>
2019-07-09 00:08:53 -0400

    moved back files in main folder and improved speed of code

--- invmass.py
+++ invmass.py
@@ -5,6 +5,7 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collect
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from libraries.getDataset import get_data_chain
 from importlib import import_module
+from glob import glob
 
 class DiMuonMass(Module):
 	def __init__(self):
@@ -16,26 +17,27 @@ class DiMuonMass(Module):
 		M_max = 3.5
 		M_min = 2.5
 		binw = (M_max-M_min)/nbins
-		self.hM = r.TH1F("M", "DiMuon Mass", nbins, M_min, M_max)
+		self.hM = r.TH1F("hM", "DiMuon Mass", nbins, M_min, M_max)
 		self.hM.GetXaxis().SetTitle("m(#mu#mu) [GeV/c^{2}]")
 		self.hM.GetYaxis().SetTitle("Candidates / ("+str(binw)+"GeV/c^{2})")
 		self.hM.SetMinimum(0)
 		self.addObject(self.hM)
 
 	def analyze(self, event):
-		#muons = Collection(event, "Muon")
-		# eventSum = r.TLorentzVector()
+		muons = Collection(event, "Muon")
+		eventSum = r.TLorentzVector()
 
-		# #select events with at least 2 muons, opposite charge
-		# mu_plus = filter(lambda x: x.charge==1, muons)
-		# mu_minus = filter(lambda x: x.charge==-1, muons)
+		#select events with at least 2 muons, opposite charge
+		mu_plus = filter(lambda x: x.charge==1, muons)
+		mu_minus = filter(lambda x: x.charge==-1, muons)
 
-		# if len(mu_plus)>0 and len(mu_minus)>0:
-		# 	eventSum = mu_plus[0].p4()+mu_minus[0].p4()
-		# 	self.hM.Fill(eventSum.M())
+		if len(mu_plus)>0 and len(mu_minus)>0:
+			eventSum = mu_plus[0].p4()+mu_minus[0].p4()
+			self.hM.Fill(eventSum.M())
 
 		return True
 
 from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import PostProcessor
-p=PostProcessor(".",["5389C378-DE55-B145-992A-E9E72E8AA489.root"],cut="",branchsel="keep_and_drop.txt",modules=[DiMuonMass()],histFileName="hM.root",histDirName="hists")
+l=glob("/mnt/hadoop/cms/store/data/Run2018A/MuOnia/NANOAOD/Nano14Dec2018-v1/20000/*")
+p=PostProcessor(".",l,cut="",branchsel="keep_and_drop.txt",modules=[DiMuonMass()],histFileName="hM.root",histDirName="hists", noOut=True) #noOut prevents from writing cut tree to disk
 p.run()
